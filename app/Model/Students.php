2945f17e5886d1a934c412b8fecae42898f7f886
Natasha Krasheninnikova <nk110905@mail.ru>
2025-09-17 12:49:23 +0700

    model students commit

--- app/Model/Students.php
+++ app/Model/Students.php
@@ -2,5 +2,25 @@
 
 namespace Model;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
+class Students extends Model
+{
+    protected $table = 'students';
+    protected $primaryKey = 'student_id';
+
+    // 3. Отключаем автоматические поля времени (created_at, updated_at)
+    public $timestamps = false;
+
+    // 4. Поля, которые можно массово заполнять (например, через create())
+    protected $fillable = [
+        'lastname',
+        'firstname',
+        'patronymic',
+        'gender',
+        'birth_date',
+        'registration_address',
+        'course_number',
+        'group_id'
+    ];
+}
