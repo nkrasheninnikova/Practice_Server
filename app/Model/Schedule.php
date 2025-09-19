053d1cc6ef262ba10218ca635e3b3e59d20e4bd7
Natasha Krasheninnikova <nk110905@mail.ru>
2025-09-19 22:23:24 +0700

    model shedule commit

--- app/Model/Schedule.php
+++ app/Model/Schedule.php
@@ -2,5 +2,19 @@
 
 namespace Model;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
+class Schedule extends Model
+{
+    protected $table = 'schedule';
+    protected $primaryKey = 'shedule_id';
+    public $timestamps = false;
+    protected $fillable = [
+        'discipline_id',
+        'group_id',
+        'teacher_id',
+        'class_date',
+        'class_time',
+        'classroom'
+    ];
+}
