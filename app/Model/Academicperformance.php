ab813a9ddf27f97d11582f756e385a8b3b08e4c6
Natasha Krasheninnikova <nk110905@mail.ru>
2025-09-19 22:30:46 +0700

    model  commit

--- app/Model/Academicperformance.php
+++ app/Model/Academicperformance.php
@@ -2,5 +2,18 @@
 
 namespace Model;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
+class Academicperformance extends Model
+{
+    protected $table = 'academicperformance';
+    protected $primaryKey = 'performance_id';
+    public $timestamps = false;
+    protected $fillable = [
+        'student_id',
+        'discipline_id',
+        'grade',
+        'hours_spent',
+        'assessment_type'
+    ];
+}
