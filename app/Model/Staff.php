3dc81e008e50dd5358d52e1467de8fc15a006a22
Natasha Krasheninnikova <nk110905@mail.ru>
2025-09-19 22:20:52 +0700

    model staff commit

--- app/Model/Staff.php
+++ app/Model/Staff.php
@@ -4,3 +4,17 @@ namespace Model;
 
 use Illuminate\Database\Eloquent\Model;
 
+class Staff extends Model
+{
+    protected $table = 'staff';
+    protected $primaryKey = 'staff_id';
+    public $timestamps = false;
+    protected $fillable = [
+        'role_staff',
+        'lastname',
+        'firstname',
+        'patronymic',
+        'login',
+        'password'
+    ];
+}
