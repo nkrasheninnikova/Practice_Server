df0ad476ba634542c5f30cac81b29ded2dfe4d6e
Наталья Крашенинникова <gr431_krnvi@lab-402-4.tomtit-tomsk.ru>
2025-09-24 16:56:28 +0700

    discipline_add commit

--- views/site/discipline_add.php
+++ views/site/discipline_add.php
@@ -1,21 +1,73 @@
-<h2>Добавление Дисциплины</h2>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Добавление дисциплины</title>
+    <link rel="stylesheet" href="/public/css/style.css">
+</head>
+<body>
+<div class="wrapper">
+    <header>
+        <div class="header">
+            <div class="logo">Logo</div>
+        </div>
+    </header>
 
-<form method="POST" action="">
-    <label>
-        Номер группы
-        <input type="number" name="group_number" required>
-    </label>
-    <label>
-        Специальность
-        <input type="text" name="specialization" required>
-    </label>
-    <label>
-        Дисциплина
-        <input type="text" name="discipline" required>
-    </label>
-    <label>
-        Общее количество часов
-        <input type="number" name="total_number_of_hours" required>
-    </label>
-    <button type="submit">Добавить</button>
-</form>
+    <main>
+        <section class="adding-discipline-form-block">
+            <div class="adding-discipline-form-center">
+                <div class="adding-discipline-form-text">
+                    <div class="adding-discipline-form-header">
+                        <p>&larr;</p>
+                        <p>Добавление дисциплины</p>
+                    </div>
+                    <form class="adding-discipline-form" method="post">
+                        <div class="adding-discipline-form-element">
+                            <label>
+                                <input type="text" name="group number" placeholder="|Номер группы">
+                            </label>
+                        </div>
+                        <div class="adding-discipline-form-element">
+                            <label>
+                                <input type="text" name="specialization" placeholder="|Специальность">
+                            </label>
+                        </div>
+                        <div class="adding-discipline-form-element">
+                            <label>
+                                <input type="text" name="discipline" placeholder="|Дисциплина">
+                            </label>
+                        </div>
+                        <div class="adding-discipline-form-element">
+                            <label>
+                                <input type="text" name="total number of hours" placeholder="|Общее количество часов">
+                            </label>
+                        </div>
+                        <div class="adding-discipline-form-element-button">
+                            <button type="submit">Добавить</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </section>
+    </main>
+
+    <footer class="footer-section">
+        <div class="footer-block">
+            <div class="footer-block-menu">
+                <div class="footer-block-menu-text">
+                    <p>Информация</p>
+                    <p>Информация</p>
+                    <p>Информация</p>
+                </div>
+                <div class="footer-block-menu-logo">Logo</div>
+                <div class="footer-block-menu-icons">
+                    <img src="img/icons-vk.png" alt="icons-vk">
+                    <img src="img/icons-telegram.png" alt="icons-telegram">
+                    <img src="img/icons-mail.png" alt="icons-mail">
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
+</body>
+</html>
