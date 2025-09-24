602b06b6c7ad6f3325d82edff545c7b37b826600
Наталья Крашенинникова <gr431_krnvi@lab-402-4.tomtit-tomsk.ru>
2025-09-24 17:10:42 +0700

    academicPerformance_add commit

---
+++ views/site/academicPerformance_add.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Добавление фиксации успеваемости</title>
+    <link rel="stylesheet" href="/public/css/style.css">
+</head>
+<body>
+<div class="wrapper">
+    <header>
+        <div class="header">
+            <div class="logo">Logo</div>
+        </div>
+    </header>
+
+    <main>
+        <section class="adding-of-academic-perfomance">
+            <div class="adding-of-academic-performance-center">
+                <div class="adding-of-academic-performance-text">
+                    <div class="adding-of-academic-performance-header">
+                        <p>&larr;</p>
+                        <p>Фиксация успеваемости</p>
+                    </div>
+
+                    <form class="adding-of-academic-performance-form" method="post">
+                        <div class="adding-of-academic-perfomance-element">
+                            <label>
+                                <input type="text" name="group number" placeholder="|Номер группы">
+                            </label>
+                        </div>
+                        <div class="adding-of-academic-perfomance-element">
+                            <label>
+                                <input type="text" name="specialization" placeholder="|Специальность">
+                            </label>
+                        </div>
+                        <div class="adding-of-academic-perfomance-element">
+                            <label>
+                                <input type="text" name="discipline" placeholder="|Дисциплина">
+                            </label>
+                        </div>
+                        <div class="adding-of-academic-perfomance-element">
+                            <label>
+                                <input type="text" name="estimation" placeholder="|Оценка">
+                            </label>
+                        </div>
+                        <div class="adding-of-academic-perfomance-element">
+                            <label>
+                                <input type="text" name="type of control" placeholder="|Вид контроля">
+                            </label>
+                        </div>
+                        <div class="adding-of-academic-perfomance-element-button">
+                            <button type="submit">Добавить</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </section>
+    </main>
+
+    <footer class="footer-section">
+        <div class="footer-block">
+            <div class="footer-block-menu">
+                <div class="footer-block-menu-text">
+                    <p>Информация</p>
+                    <p>Информация</p>
+                    <p>Информация</p>
+                </div>
+                <div class="footer-block-menu-logo">Logo</div>
+                <div class="footer-block-menu-icons">
+                    <img src="img/icons-vk.png" alt="icons-vk">
+                    <img src="img/icons-telegram.png" alt="icons-telegram">
+                    <img src="img/icons-mail.png" alt="icons-mail">
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
+</body>
+</html>
+
