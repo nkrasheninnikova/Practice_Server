046e5da7f54f5f0288fb19447cef37be59a012c0
Наталья Крашенинникова <gr431_krnvi@lab-402-4.tomtit-tomsk.ru>
2025-09-24 15:27:44 +0700

    student_add commit

--- views/site/student_add.php
+++ views/site/student_add.php
@@ -1,29 +1,85 @@
 <h2>Добавление студента</h2>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Добавление студента</title>
+    <link rel="stylesheet" href="/public/css/style.css">
+</head>
+<body>
+<div class="wrapper">
+    <header>
+        <div class="header">
+            <div class="logo">Logo</div>
+        </div>
+    </header>
 
-<form method="POST" action="">
-    <label>
-        Фамилия
-        <input type="text" name="lastname" required>
-    </label>
-    <label>
-        Имя
-        <input type="text" name="firstname" required>
-    </label>
-    <label>
-        Отчество
-        <input type="text" name="patronymic" required>
-    </label>
-    <label>
-        Номер группы
-        <input type="number" name="group_number" required>
-    </label>
-    <label>
-        Дата рождения
-        <input type="date" name="birth_date" required>
-    </label>
-    <label>
-        Адресс прописки
-        <input type="text" name="registration_address" required>
-    </label>
-    <button type="submit">Добавить</button>
-</form>
+    <main>
+        <section class="adding-student-form-block">
+            <div class="adding-student-form-center">
+                <div class="adding-student-form-text">
+                    <div class="adding-student-form-header">
+                        <p>&larr;</p>
+                        <p>Добавление студента</p>
+                    </div>
+
+                    <form class="adding-student-form" method="post">
+                        <div class="adding-student-form-element">
+                            <label>
+                                <input type="text" name="surname" placeholder="|Фамилия">
+                            </label>
+                        </div>
+                        <div class="adding-student-form-element">
+                            <label>
+                                <input type="text" name="name" placeholder="|Имя">
+                            </label>
+                        </div>
+                        <div class="adding-student-form-element">
+                            <label>
+                                <input type="text" name="patronymic" placeholder="|Отчество">
+                            </label>
+                        </div>
+                        <div class="adding-student-form-element">
+                            <label>
+                                <input type="text" name="group number" placeholder="|Номер группы">
+                            </label>
+                        </div>
+                        <div class="adding-student-form-element">
+                            <label>
+                                <input type="text" name="date of birth" placeholder="|Дата рожджения">
+                            </label>
+                        </div>
+                        <div class="adding-student-form-element">
+                            <label>
+                                <input type="text" name="registration address" placeholder="|Адресс прописки">
+                            </label>
+                        </div>
+                        <div class="adding-student-form-element-button">
+                            <button type="submit">Добавить</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </section>
+    </main>
+
+    <footer class="footer-section">
+        <div class="footer-block">
+            <div class="footer-block-menu">
+                <div class="footer-block-menu-text">
+                    <p>Информация</p>
+                    <p>Информация</p>
+                    <p>Информация</p>
+                </div>
+                <div class="footer-block-menu-logo">Logo</div>
+                <div class="footer-block-menu-icons">
+                    <img src="img/icons-vk.png" alt="icons-vk">
+                    <img src="img/icons-telegram.png" alt="icons-telegram">
+                    <img src="img/icons-mail.png" alt="icons-mail">
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
+</body>
+</html>
